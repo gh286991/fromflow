83c2216fc6f632802c76e04044cc8f1a58f06e50
Tom <gh286991@gmail.com>
2021-11-02 14:43:53 +0800

    ifarme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import FlowPageContainer from "./modules/Flow/component/flowPage" ;
+import FlowPageContainer from "./modules/Flow/component/flowPage";
 
 function App() {
   return (
-    <div >
-      <FlowPageContainer  /> 
+    <div>
+      <FlowPageContainer />
     </div>
   );
 }
